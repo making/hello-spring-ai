5a45db8f77f58436054070dc28ea3547c1a21f27
Toshiaki Maki <makingx@gmail.com>
2025-02-01 03:20:48 +0900

    fix

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.hello;
 
+import java.util.stream.Collectors;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class HelloController {
+
 	private final ChatClient chatClient;
 
 	public HelloController(ChatClient.Builder chatClientBuilder) {
@@ -15,8 +21,18 @@ public class HelloController {
 
 	@GetMapping(path = "/")
 	public String hello(@RequestParam(defaultValue = "Tell me a joke") String prompt) {
-		return this.chatClient.prompt()
-				.messages()
-				.user(prompt).call().content();
+		return this.chatClient.prompt().messages().user(prompt).call().content();
 	}
+
+	@PostMapping(path = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> helloStream(@RequestBody String prompt) {
+		return this.chatClient.prompt()
+			.messages()
+			.user(prompt)
+			.stream()
+			.content()
+			.windowUntil(s -> s.endsWith(".") || s.endsWith("。"))
+			.flatMap(flux -> flux.collect(Collectors.joining()));
+	}
+
 }
