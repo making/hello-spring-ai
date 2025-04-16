9fc610be57664f69e4e71abe55ee9894aaaefcbe
Toshiaki Maki <makingx@gmail.com>
2025-04-16 12:13:06 +0900

    Simplify http log format

--- src/main/java/com/example/hello/HelloSpringAiApplication.java
+++ src/main/java/com/example/hello/HelloSpringAiApplication.java
@@ -1,13 +1,13 @@
 package com.example.hello;
 
 import am.ik.spring.http.client.RetryableClientHttpRequestInterceptor;
-import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.backoff.FixedBackOff;
+import org.zalando.logbook.HttpLogFormatter;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @SpringBootApplication
 public class HelloSpringAiApplication {
@@ -21,6 +21,11 @@ public class HelloSpringAiApplication {
 			LogbookClientHttpRequestInterceptor logbookClientHttpRequestInterceptor) {
 		return restClientBuilder -> restClientBuilder.requestInterceptor(logbookClientHttpRequestInterceptor)
 			.requestInterceptor(new RetryableClientHttpRequestInterceptor(new FixedBackOff(1000, 2)));
+	}
+
+	@Bean
+	public HttpLogFormatter simpleHttpLogFormatter() {
+		return new SimpleHttpLogFormatter();
 	}
 
 }
